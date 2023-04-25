7ff939fcc94146d268762f997c8efb6324de3fab
Gerald Thwala <thwala.gerald006@gmail.com>
2023-04-25 13:23:35 +0200

    Create program.cs
    Playing around with basic Console.WriteLine commands

---
+++ program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      /*We are making ASCII art*/
+      Console.WriteLine(" .-."); 
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |"); 
+      Console.WriteLine("|   |");  
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("\n");
+      Console.WriteLine("The Ghost Of MIP");
+    
+     }
+  }
+}
